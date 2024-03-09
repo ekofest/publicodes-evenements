50d6c745a7dc81a0c35b13735c0e6d99531f9d66
Clément <clem.auger@hotmail.fr>
2024-03-09 16:36:59 +0100

    fetat: set up testPersonas

---
+++ tests/commons.js
@@ -0,0 +1,210 @@
+import c from "ansi-colors"
+import yargs from "yargs"
+import { readFile } from "fs/promises"
+import { serializeUnit } from "publicodes"
+import rules, { personas } from "publicodes-evenements"
+
+export function getArgs() {
+  return yargs(process.argv.slice(2))
+    .version(false)
+    .usage("Compare local and prod personas results\n\nUsage: $0 [options]")
+    .option("markdown", {
+      alias: "m",
+      type: "boolean",
+      description: "Prints the result in a Markdown table format.",
+    })
+    .option("persona", {
+      alias: "p",
+      type: "string",
+      description: "Test only one persona",
+    })
+    .help("h")
+    .alias("h", "help").argv
+}
+
+export function getLocalRules() {
+  return readFile("./publicodes-evenements.model.json", {
+    encoding: "utf8",
+  })
+    .then((res) => JSON.parse(res))
+    .catch((e) => {
+      console.error(`No local rules found`)
+      console.error(e.message)
+      process.exit(1)
+    })
+}
+
+export function getLocalPersonas() {
+  return readFile("./publicodes-evenements.personas.json", {
+    encoding: "utf8",
+  })
+    .then((res) => JSON.parse(res))
+    .catch((e) => {
+      console.error(`No local personas found`)
+      console.error(e.message)
+      process.exit(1)
+    })
+}
+
+export function getLatestRules() {
+  return rules
+}
+
+export function getLatestPersonas() {
+  return personas
+}
+
+export function printResults({ markdownHeader, results, nbTests, markdown }) {
+  if (results.length === 1 && results[0].type === "error") {
+    // An error occured while trying to set the situation
+    if (markdown) {
+      console.log(`
+An error occured while testing the model:
+
+~~~${results[0].message}
+~~~
+`)
+    } else {
+      console.log(`${c.red("(err)")} An error occured while testing the model:`)
+      console.log(`${results[0].message}\n`)
+    }
+    // TODO: remove this when the production use the latest version
+    // process.exit(1)
+    return
+  }
+
+  if (markdown) {
+    console.log()
+    console.log(markdownHeader)
+    console.log("|:-----|:------|:------|:-------|")
+  }
+
+  const fails = []
+
+  results.sort((a, b) => a.rule.localeCompare(b.rule))
+  let nbDiff = 0
+
+  for (const result of results) {
+    if (result.type === "warning") {
+      if (!markdown) {
+        console.log(
+          `${c.yellow("(warn)")} ${c.magenta(result.rule)}: ${result.msg}`
+        )
+      } else {
+        // NOTE: for now we don't need to print the warning in the markdown
+        // console.log(
+        //   `| ${c.magenta(result.rule)} | | | | (warning) ${result.msg} |`
+        // )
+      }
+
+      continue
+    }
+    const actualRounded =
+      result.actual.nodeValue == null
+        ? result.actual.nodeValue
+        : Math.fround(result.actual.nodeValue)
+    const actualUnit = serializeUnit(result.actual.unit)
+    const expectedRounded =
+      result.expected.nodeValue == null
+        ? result.expected.nodeValue
+        : Math.fround(result.expected.nodeValue)
+    const expectedUnit = serializeUnit(result.expected.unit)
+
+    const diff =
+      isFinite(actualRounded) && isFinite(expectedRounded)
+        ? actualRounded - expectedRounded
+        : 0
+
+    if (diff !== 0) {
+      nbDiff++
+      const diffPercent = Math.abs(Math.round((diff / expectedRounded) * 100))
+
+      if (markdown) {
+        console.log(
+          fmtGHActionErr(
+            actualRounded,
+            actualUnit,
+            expectedRounded,
+            expectedUnit,
+            diff,
+            diffPercent,
+            result.rule
+          )
+        )
+      } else {
+        fails.push(
+          fmtCLIErr(
+            actualRounded,
+            actualUnit,
+            expectedRounded,
+            expectedUnit,
+            diff,
+            diffPercent,
+            result.rule,
+            result.message
+          )
+        )
+      }
+    }
+  }
+
+  if (markdown) {
+    if (nbDiff === 0) {
+      console.log(`✅ _Aucune différence détectée sur **${nbTests}** tests_`)
+    }
+  }
+
+  if (!markdown) {
+    const nbFails = fails.length
+    fails.forEach((fail) => console.log(fail))
+    if (nbFails > 0) {
+      console.log(`\n${c.red("DIFF")} ${nbFails}/${nbTests}`)
+    } else {
+      console.log(`\n${c.green("OK")} ${nbTests}/${nbTests}\n`)
+    }
+  }
+}
+
+function formatValue(value) {
+  return value === null ? "null" : Math.fround(value).toLocaleString("en-us")
+}
+
+function fmtCLIErr(
+  actual,
+  actualUnit,
+  expected,
+  expectedUnit,
+  diff,
+  diffPercent,
+  rule,
+  message
+) {
+  const color = diffPercent <= 1 ? c.yellow : c.red
+  const sign = diff > 0 ? "+" : diff < 0 ? "-" : ""
+  return `${c.magenta(rule)}: ${color(
+    "(" + c.bold(sign + diffPercent) + "%)"
+  )} ${message ? `\n${message}` : ""}\n  ${
+    c.dim("actual: ") + formatValue(actual)
+  } ${c.dim.italic(actualUnit ?? "")}\n  ${
+    c.dim("expected: ") + formatValue(expected)
+  } ${c.dim.italic(expectedUnit ?? "")}`
+}
+
+function fmtGHActionErr(
+  actual,
+  actualUnit,
+  expected,
+  expectedUnit,
+  diff,
+  diffPercent,
+  name
+) {
+  // const color =
+  //   diffPercent <= 1 ? 'sucess' : diffPercent > 5 ? 'critical' : 'important'
+  // const sign = diff > 0 ? '%2B' : '-'
+  return `| ${name} | ${formatValue(actual)} ${
+    actualUnit ? `_${actualUnit}_` : ""
+  } | ${formatValue(expected)} ${expectedUnit ? `_${expectedUnit}_` : ""} | **${
+    diff > 0 ? "+" : "-"
+  }${diffPercent}%** |`
+}
---
+++ tests/testPersonas.js
@@ -0,0 +1,85 @@
+import c from "ansi-colors"
+import Engine from "publicodes"
+import { disabledLogger } from "@publicodes/tools"
+
+import {
+  getArgs,
+  getLocalRules,
+  getLocalPersonas,
+  getLatestRules,
+  getLatestPersonas,
+  printResults,
+} from "./commons.js"
+import safeGetSituation from "./helpers/safeGetSituation.js"
+
+/**
+ * Compares the value of all the rules between the local and the prod (or specified) version
+ * with the situation of the specified persona (default: all personas).
+ **/
+
+const { markdown, persona } = getArgs()
+
+const localRules = await getLocalRules()
+let localPersonas = await getLocalPersonas()
+
+const prodRules = getLatestRules()
+let prodPersonas = getLatestPersonas()
+
+if (persona && persona in localPersonas && persona in prodPersonas) {
+  localPersonas = { [persona]: localPersonas[persona] }
+  prodPersonas = { [persona]: prodPersonas[persona] }
+}
+
+const localEngine = new Engine(localRules, { logger: disabledLogger })
+const prodEngine = new Engine(prodRules, { logger: disabledLogger })
+
+const nbRules = Object.keys(localRules).length
+
+for (const personaName in localPersonas) {
+  const { situation: localSituation } = localPersonas[personaName]
+  const { situation: prodSituation } = prodPersonas[personaName]
+  const results = []
+
+  if (markdown) {
+    console.log(`#### ${localPersonas[personaName].nom}\n`)
+  } else {
+    console.log(
+      `[ Test persona ${c.magenta(personaName)} regression against ${c.green(
+        "latest"
+      )} ]\n`
+    )
+  }
+
+  try {
+    const safeSituation = safeGetSituation({
+      situation: localSituation || {},
+      parsedRulesNames: Object.keys(localEngine.getParsedRules()),
+    })
+    const safeProdSituation = safeGetSituation({
+      situation: prodSituation || {},
+      parsedRulesNames: Object.keys(prodEngine.getParsedRules()),
+    })
+    localEngine.setSituation(safeSituation)
+    prodEngine.setSituation(safeProdSituation)
+  } catch (e) {
+    printResults({ results: [{ type: "error", message: e.message }], markdown })
+    continue
+  }
+
+  for (const rule in localRules) {
+    if (!(rule in prodRules)) {
+      continue
+    }
+
+    const local = localEngine.evaluate(rule)
+    const prod = prodEngine.evaluate(rule)
+    results.push({ type: "result", rule, actual: local, expected: prod })
+  }
+
+  printResults({
+    markdownHeader: `| Règle | PR | Latest | Δ (%) |`,
+    results,
+    nbTests: nbRules,
+    markdown,
+  })
+}
