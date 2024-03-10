5c547edafb6aabbf78a85cc9b5777cc301f87429
Clément <clem.auger@hotmail.fr>
2024-03-10 01:17:01 +0100

    feat: set very simple personas

--- scripts/compile-personas.js
+++ scripts/compile-personas.js
@@ -2,14 +2,11 @@ import { parse } from "yaml"
 import { readFileSync } from "fs"
 
 /**
- * Parses the festivals-type.yaml file
+ * Parses the personas.yaml file
  * TODO: checks that personas definition are valid.
- *
- * @param {Record<string, import("publicodes").Rule>} model - The publicodes model.
- *
  */
-export default function getPersonas(model) {
-  const personas = parse(readFileSync("festivals-type.yaml", "utf-8"))
+export default function getPersonas() {
+  const personas = parse(readFileSync("personas.yaml", "utf-8"))
   let error = false
 
   if (error) {
